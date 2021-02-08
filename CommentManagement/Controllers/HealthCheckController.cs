fa267e19ff5cdc2bc2fff86cb2f51f4e28357fe5
Anagh Hegde <Anagh.HV@nasdaq.com>
2021-02-08 19:20:11 +0530

    Added healthcheck

---
+++ CommentManagement/Controllers/HealthCheckController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CommentManagement.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        private static readonly string _healthCheck = "HealthCheck Success";
+
+        private readonly ILogger<HealthCheckController> _logger;
+
+        public HealthCheckController(ILogger<HealthCheckController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public String Get()
+        {
+            return _healthCheck;
+        }
+    }
+}
